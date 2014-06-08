7edbfd2d3d13f2a3d9645c0efe188c46c58d1ff5
mwatha <mwathabwanali@gmail.com>
2014-06-09 00:40:40 +0200

    adding new report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,8 +3,22 @@ class ReportsController < ApplicationController
     if not params[:start_date].blank? and not params[:end_date].blank?
       @start_date = params[:start_date].to_date
       @end_date = params[:end_date].to_date
+      case params[:age]
+        when 'All'
+          age = [0,1000]
+          @age_group = "All patients"
+        when '0,0'
+          age = [0,0]
+          @age_group = "Patients under one year old when starting"
+        when '1 to 14'
+          age = [1,14]
+          @age_group = "Patients between 1 yr and under 15 yrs old when starting"
+        when '>= 15'
+          age = [15,1000]
+          @age_group = "Patients over 15 years old when starting"
+      end
 
-      @total_registered = Report.total_registered(@start_date, @end_date)
+      @total_registered = Report.total_registered(@start_date, @end_date, age)
       @followup_months = {}
       unless @total_registered.blank?
         patients_to_follow = @total_registered.keys
--- app/models/report.rb
+++ app/models/report.rb
@@ -301,13 +301,14 @@ ORDER BY clinic ASC"])
     return demographics
   end
 
-  def self.total_registered(start_date, end_date)
+  def self.total_registered(start_date, end_date, age)
     total_registered = {}
 
     result = Encounter.find_by_sql("SELECT * FROM earliest_start_date e
       INNER JOIN person p ON p.person_id = e.patient_id AND p.voided = 0 
       WHERE e.earliest_start_date BETWEEN '#{start_date}' AND '#{end_date}'
       AND (LEFT(e.date_enrolled,10) = e.earliest_start_date) 
+      AND age_at_initiation BETWEEN #{age.first} AND #{age.last} 
       GROUP BY e.patient_id;")
 
     unless result.blank?

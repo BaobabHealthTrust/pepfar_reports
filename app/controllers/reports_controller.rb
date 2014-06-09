65b1aadd340d64876187bf299883002d88ce0870
mwatha <mwathabwanali@gmail.com>
2014-06-09 09:47:06 +0200

    merging

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -32,4 +32,37 @@ class ReportsController < ApplicationController
 
   end
 
+  def pregnant_started
+    if not params[:start_date].blank? and not params[:end_date].blank?
+      @start_date = params[:start_date].to_date
+      @end_date = params[:end_date].to_date
+      case params[:age]
+        when 'All'
+          age = [0,1000]
+          @age_group = "All patients"
+        when '0,0'
+          age = [0,0]
+          @age_group = "Patients under one year old when starting"
+        when '1 to 14'
+          age = [1,14]
+          @age_group = "Patients between 1 yr and under 15 yrs old when starting"
+        when '>= 15'
+          age = [15,1000]
+          @age_group = "Patients over 15 years old when starting"
+      end
+
+      @total_registered = Report.total_registered_pregnant(@start_date, @end_date, age)
+      @followup_months = {}
+      unless @total_registered.blank?
+        patients_to_follow = @total_registered.keys
+        (1.upto(12)).each do |m|
+          next unless m == 12
+          followup_start_date = @start_date + m.month
+          @followup_months[followup_start_date] = Report.followup_months(patients_to_follow,followup_start_date)
+        end
+      end
+    end
+
+  end
+
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -380,4 +380,36 @@ ORDER BY clinic ASC"])
     end
   end
 
+  def self.total_registered_pregnant(start_date, end_date, age)
+    total_registered = {}
+
+    result = Encounter.find_by_sql("SELECT * FROM earliest_start_date e
+      INNER JOIN person p ON p.person_id = e.patient_id AND p.voided = 0 AND p.gender = 'F'
+      LEFT JOIN obs ON obs.person_id = p.person_id = obs.person_id 
+      AND obs.voided = 0 AND obs.concept_id = 7563 AND obs.value_coded = 1755
+      LEFT JOIN patient_pregnant_obs preg ON preg.person_id = e.patient_id
+      AND preg.concept_id = 1065 AND LEFT(preg.obs_datetime,10) = e.earliest_start_date
+      WHERE e.earliest_start_date BETWEEN '#{start_date}' AND '#{end_date}'
+      AND (LEFT(e.date_enrolled,10) = e.earliest_start_date) 
+      AND age_at_initiation BETWEEN #{age.first} AND #{age.last} 
+      GROUP BY e.patient_id;")
+
+    unless result.blank?
+      result.each do |r|
+        gender =  r.gender.upcase rescue nil
+        next if gender.blank?
+        if total_registered[r.patient_id].blank? 
+          total_registered[r.patient_id] = []
+
+          total_registered[r.patient_id] = {
+            :earliest_start_date =>  r.earliest_start_date,
+            :age_at_initiation => r.age_at_initiation,
+            :gender => gender
+          }
+        end
+      end
+    end
+    return total_registered
+  end
+
 end
